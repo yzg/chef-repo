690faa225093e30a2c595fdf7593c13466285238
yzg <shirasagi13@gmail.com>
2017-05-13 20:12:30 +0900

    update docker

--- cookbooks/docker/recipes/default.rb
+++ cookbooks/docker/recipes/default.rb
@@ -1,26 +1,34 @@
 
-package [
-  'apt-transport-https',
-  'ca-certificates',
-  'curl',
-  'software-properties-common'
-]
-
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo apt-key fingerprint 0EBFCD88
-
-apt_repository 'docker' do
-  uri 'https://download.docker.com/linux/ubuntu'
-  distribution 'xenial'
-  arch 'amd64'
-  components ['stable']
-end
-
-include_recipe 'apt'
-
-package 'docker-ce'
-
-service 'docker' do
-  action [:enable, :start]
+case node[:platform]
+when 'windows' then
+  chocolatey_package [
+    'docker',
+    'docker-machine',
+  ]
+when 'ubuntu' then
+  package [
+    'apt-transport-https',
+    'ca-certificates',
+    'curl',
+    'software-properties-common'
+  ]
+  
+  # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  # sudo apt-key fingerprint 0EBFCD88
+  
+  apt_repository 'docker' do
+    uri 'https://download.docker.com/linux/ubuntu'
+    distribution 'xenial'
+    arch 'amd64'
+    components ['stable']
+  end
+  
+  include_recipe 'apt'
+  
+  package 'docker-ce'
+  
+  service 'docker' do
+    action [:enable, :start]
+  end
 end
 
